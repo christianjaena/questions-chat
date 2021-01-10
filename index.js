15363a314c17d38ad79bdb58f58026e217e59334
Christian Jaena <christian.jaena@msugensan.edu.ph>
2021-01-10 21:25:33 +0800

    changed env PORT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const app = require('express')()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
+const PORT = process.env.PORT || 5000;
 
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
@@ -74,6 +75,6 @@ io.on('connection', socket => {
 	})
 })
 
-http.listen(3000, () => {
-	console.log('listening on port 3000')
+http.listen(PORT, () => {
+		console.log(`listening on port ${PORT}`)
 })
