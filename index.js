2639a8a02164bf194b6f198b04501fdbd19232f1
christianjaena <christian.jaena@msugensan.edu.ph>
2021-01-11 15:56:40 +0800

    added disconnection and emote

--- index.js
+++ index.js
@@ -1,24 +1,29 @@
-const app = require('express')()
+const express = require('express')
+const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000
 const questions = require('./questions')
+const fs = require('fs')
+const path = require('path')
+const { reduce } = require('./questions')
 
+app.use(express.static('./'))
 app.get('/', (req, res) => {
 	res.sendFile(__dirname + '/index.html')
 })
 
 function getQuestion() {
 	let randomNum = Math.floor(Math.random() * questions.length)
-	console.log(randomNum)
 	let pick = questions[randomNum]
-	console.log(pick)
 	questions.splice(randomNum, 1)
-	console.log(questions)
 	return pick
 }
 
 io.on('connection', socket => {
+	socket.on('emote', emote => {
+		io.emit('img', emote)
+	})
 
 	socket.on('chat message', msg => {
 		io.emit('chat message', msg)
@@ -26,18 +31,20 @@ io.on('connection', socket => {
 
 	socket.on('user', userName => {
 		io.emit('user', userName)
+		socket.on('disconnect', () => {
+			socket.broadcast.emit('disconnection', userName)
+		})
 	})
 
 	socket.on('question', () => {
 		if (questions.length > 0) {
 			io.emit('question', getQuestion())
 		} else {
-			io.emit('question', "No questions left")
+			io.emit('question', 'No questions left')
 		}
 	})
-
 })
 
 http.listen(PORT, () => {
-		console.log(`listening on port ${PORT}`)
+	console.log(`listening on port ${PORT}`)
 })
